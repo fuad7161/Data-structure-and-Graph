96c4bca6a7b4659c531758a239d4e24ef4cee7f5
fuadul hasan <53356319+fuad7161@users.noreply.github.com>
2020-12-12 14:59:19 +0600

    Add files via upload

---
+++ Dynamic Programming Algorithm/LCS1.cpp
@@ -0,0 +1,130 @@
+//Author: Fuadul Hasan(fuadul202@gmail.com)
+//BSMRSTU,Gopalganj
+//#include<bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <random>
+#include <cassert>
+#include <cstring>
+#include <sstream>
+#include <complex>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+//int input........
+template <typename T> inline void readline(T &n) {n = 0; int f = 1; register int ch = getchar();
+ for (; !isdigit(ch); ch = getchar()) if (ch == '-') f = -1;for (; isdigit(ch); 
+ch = getchar()) n = (n << 3) + (n << 1) + ch - '0';n = n * f;}
+template <typename T, typename TT> inline void readline(T &n, TT &m) { readline(n); readline(m); }
+template <typename T, typename TT, typename TTT> inline void readline(T &n, TT &m, TTT &l) { readline(n, m); readline(l);}
+// long long input........
+template <typename T> inline void readlinel(T &n) {n = 0; long long f = 1; register long long ch = getchar();
+ for (; !isdigit(ch); ch = getchar()) if (ch == '-') f = -1;for (; isdigit(ch); 
+ch = getchar()) n = (n << 3) + (n << 1) + ch - '0';n = n * f;}
+template <typename T, typename TT> inline void readlinel(T &n, TT &m) { readlinel(n); readlinel(m); }
+template <typename T, typename TT, typename TTT> inline void readlinel(T &n, TT &m, TTT &l) { readlinel(n, m); readlinel(l);}
+//debug..........
+#define error(args...) {vector<string>_v=split(#args,',');err(_v.begin(),args);cout<<endl;}
+vector<string> split(const string &s, char c) {vector<string>v; stringstream ss(s); string x;
+while (getline(ss, x, c))v.emplace_back(x); return move(v);} void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>void err(vector<string>::iterator it, T a, Args...args) {
+cout << it->substr((*it)[0] == ' ', it->length()) << " = " << a << " "; err(++it, args...);}
+//............ignore it..................//
+int dx[8]= {1,0,-1,0,-1,-1,1,1};int dy[8]= {0,1,0,-1,-1,1,-1,1}; int tc = 1;
+#define F first
+#define S second
+#define pb  push_back
+#define mp make_pair
+#define Pi atan(1)*4
+#define ll  long long int
+#define happy cin.tie(0);
+#define point(x) setprecision(x)
+#define coding ios::sync_with_stdio(false);
+#define Test printf("Case %d: ",tc++);
+#define Unique(c) (c).resize(unique(all(c))-(c).begin())
+#define vout(v) for (auto z: v) cout << z << " "; cout << endl;
+void print(int n){printf("%d\n", n);}
+void Print(int n){printf("%d ", n);}
+void print(int n,int m){printf("%d ",n);printf("%d ",m);}
+void print(int n,int m,int k){printf("%d ",n);printf("%d ",m);printf("%d ",k);}
+void print(ll n){printf("%I64d\n", n);}
+void Print(ll n){printf("%I64d ", n);}
+void print(ll n,ll m){printf("%I64d ",n);printf("%I64d ",m);}
+void print(ll n,ll m,ll k){printf("%I64d ",n);printf("%I64d ",m);printf("%I64d ",k);}
+int length(long long x){int l = 0;for(long long i=x;i;i/=10)l++;return l;}
+int length(string s){return s.size();}
+const int M           	= 1e9 + 7;
+long long pow(long long a,long long n){ll res = 1;while(n){if(n&1) res = ((res%M)*(a%M))%M;a = ((a%M)*(a%M))%M;n>>=1;}return res%M;}
+
+
+#define   pr 	pair<int, int> 
+#define   vpr 	vector<pr> 
+#define   vi   	std::vector<int>
+#define   vll  	std::vector<ll>
+#define   all(n) n.begin(),n.end()
+
+
+const int Inf           = (int)2e9 + 5;
+const ll  Lnf           = (ll)2e18 + 5;
+const int N             = 4e3 + 5;
+const int NN            = 1e6 + 5;
+
+int dp[N][N];
+
+int lcs(string a,string b){
+	int n = a.size();
+	int m = b.size();
+
+	/*for(int i=0;i<n;i++) dp[i][m] = 0;
+	for(int i=0;i<m;i++) dp[n][i] = 0;*/
+
+	for(int i=n-1;i>=0;i--){
+		for(int j=m-1;j>=0;j--){
+			if(a[i] == b[j]){
+				dp[i][j] = dp[i+1][j+1]+1;
+			}else{
+				dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+			}
+		}
+	}
+
+	for(int i=0;i<=n;i++){
+		for(int j=0;j<=m;j++){
+			cout<<dp[i][j]<<" ";
+		}cout<<endl;
+	}
+	return dp[0][0];
+}
+
+int solve() 
+{
+	//happy coding
+
+	string a,b;
+	cin>>a>>b;	
+	int x = lcs(a,b);
+	cout<<x<<endl;
+
+	return 0;
+	//error();
+}
+int main(){
+	int test = 1;
+	//readline(test);//(void)getchar();
+	while (test--)solve();return 0;
+}
---
+++ Dynamic Programming Algorithm/max_min_path_queries_on_2D_grid.cpp
@@ -0,0 +1,145 @@
+//Author: Fuadul Hasan(fuadul202@gmail.com)
+//BSMRSTU,Gopalganj
+//#include<bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <random>
+#include <cassert>
+#include <cstring>
+#include <sstream>
+#include <complex>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+//int input........
+template <typename T> inline void readline(T &n) {n = 0; int f = 1; register int ch = getchar();
+ for (; !isdigit(ch); ch = getchar()) if (ch == '-') f = -1;for (; isdigit(ch); 
+ch = getchar()) n = (n << 3) + (n << 1) + ch - '0';n = n * f;}
+template <typename T, typename TT> inline void readline(T &n, TT &m) { readline(n); readline(m); }
+template <typename T, typename TT, typename TTT> inline void readline(T &n, TT &m, TTT &l) { readline(n, m); readline(l);}
+// long long input........
+template <typename T> inline void readlinel(T &n) {n = 0; long long f = 1; register long long ch = getchar();
+ for (; !isdigit(ch); ch = getchar()) if (ch == '-') f = -1;for (; isdigit(ch); 
+ch = getchar()) n = (n << 3) + (n << 1) + ch - '0';n = n * f;}
+template <typename T, typename TT> inline void readlinel(T &n, TT &m) { readlinel(n); readlinel(m); }
+template <typename T, typename TT, typename TTT> inline void readlinel(T &n, TT &m, TTT &l) { readlinel(n, m); readlinel(l);}
+//debug..........
+#define error(args...) {vector<string>_v=split(#args,',');err(_v.begin(),args);cout<<endl;}
+vector<string> split(const string &s, char c) {vector<string>v; stringstream ss(s); string x;
+while (getline(ss, x, c))v.emplace_back(x); return move(v);} void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>void err(vector<string>::iterator it, T a, Args...args) {
+cout << it->substr((*it)[0] == ' ', it->length()) << " = " << a << " "; err(++it, args...);}
+//............ignore it..................//
+int dx[8]= {1,0,-1,0,-1,-1,1,1};int dy[8]= {0,1,0,-1,-1,1,-1,1}; int tc = 1;
+#define F first
+#define S second
+#define pb  push_back
+#define mp make_pair
+#define Pi atan(1)*4
+#define ll  long long int
+#define happy cin.tie(0);
+#define point(x) setprecision(x)
+#define coding ios::sync_with_stdio(false);
+#define Test printf("Case %d: ",tc++);
+#define Unique(c) (c).resize(unique(all(c))-(c).begin())
+#define vout(v) for (auto z: v) cout << z << " "; cout << endl;
+void print(int n){printf("%d\n", n);}
+void Print(int n){printf("%d ", n);}
+void print(int n,int m){printf("%d ",n);printf("%d ",m);}
+void print(int n,int m,int k){printf("%d ",n);printf("%d ",m);printf("%d ",k);}
+void print(ll n){printf("%I64d\n", n);}
+void Print(ll n){printf("%I64d ", n);}
+void print(ll n,ll m){printf("%I64d ",n);printf("%I64d ",m);}
+void print(ll n,ll m,ll k){printf("%I64d ",n);printf("%I64d ",m);printf("%I64d ",k);}
+int length(long long x){int l = 0;for(long long i=x;i;i/=10)l++;return l;}
+int length(string s){return s.size();}
+const int M           	= 1e9 + 7;
+long long pow(long long a,long long n){ll res = 1;while(n){if(n&1) res = ((res%M)*(a%M))%M;a = ((a%M)*(a%M))%M;n>>=1;}return res%M;}
+
+
+#define   pr 	pair<int, int> 
+#define   vpr 	vector<pr> 
+#define   vi   	std::vector<int>
+#define   vll  	std::vector<ll>
+#define   all(n) n.begin(),n.end()
+
+
+const int Inf           = (int)2e9 + 5;
+const ll  Lnf           = (ll)2e18 + 5;
+const int N             = 4e3 + 5;
+const int NN            = 1e6 + 5;
+
+int a[N][N];
+int dp[N][N];
+int n,m;
+
+bool ck(int x,int y){
+	return x>0 and y>0;
+}
+
+void make(){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			dp[i][j] = Inf;
+		}
+	}
+}
+
+void got(int x,int y){
+	if(x == n and y == m){
+		dp[n][m] = a[n-1][m-1];
+	}else{
+		if(ck(x,y)){
+		dp[x][y] = min(dp[x+1][y],dp[x][y+1])+a[x-1][y-1];
+		}else return ;
+	}
+	got(x-1,y);
+	got(x,y-1);
+
+}
+
+int solve() 
+{
+	//happy coding
+	cin>>n>>m;
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cin>>a[i][j];
+		}
+	}	
+
+	make();
+
+	got(n,m);
+
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			cout<<dp[i][j]<<" ";
+		}cout<<endl;
+	}
+
+	cout<<dp[1][1]<<endl;
+	return 0;
+	//error();
+}
+int main(){
+	int test = 1;
+	//readline(test);//(void)getchar();
+	while (test--)solve();return 0;
+}
